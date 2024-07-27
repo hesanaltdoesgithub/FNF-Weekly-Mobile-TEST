c6f6ecb769490c28089d06da344a23ea98c5f006
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-27 15:28:00 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -4,6 +4,7 @@ import sys.thread.Thread;
 import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
 import flixel.FlxGame;
+import mobile.states.CopyState;
 import flixel.FlxState;
 import flixel.util.FlxColor;
 import haxe.io.Path;
@@ -27,7 +28,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = Init; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = CopyState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
