a1dcef60ac9be9dcecd8396a0b3d8e175aa903af
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-27 15:19:21 +0800

    Update PlayState.hx

--- source/meta/states/PlayState.hx
+++ source/meta/states/PlayState.hx
@@ -131,6 +131,7 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 	public static var arrowSkin:String = '';
+	public static var qqqeb:Bool = false;
 	public static var arrowSkins:Array<String> = [];
 	public static var ratingStuff:Array<Dynamic> = [
 		['You Suck!', 0.2], //From 0% to 19%
