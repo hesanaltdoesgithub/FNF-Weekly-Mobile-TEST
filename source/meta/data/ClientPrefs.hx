5588f0f7b827112d0f183ef2c11277a8326dc773
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-27 16:04:05 +0800

    Update ClientPrefs.hx

--- source/meta/data/ClientPrefs.hx
+++ source/meta/data/ClientPrefs.hx
@@ -84,7 +84,7 @@ class ClientPrefs {
 	public static var noteSkin:String = 'Vanilla';
 	public static var noReset:Bool = false;
 	public static var healthBarAlpha:Float = 1;
-	public static var controllerMode:Bool = false;
+	public static var controllerMode:Bool = true;
 	public static var hitsoundVolume:Float = 0;
 	public static var pauseMusic:String = 'Tea Time';
 	public static var gameplaySettings:Map<String, Dynamic> = [
